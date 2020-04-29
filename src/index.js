ad9eccb6473c02b4c8b0361746cd42f86c1335f7
shadi shirinbeik <shadi.shirinbeik@gmail.com>
2020-04-29 11:23:08 +0430

    add babelrc configs to the project

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import { groupBy } from "lodash-es";
 import people from "./people";
+import "@babel/polyfill";
 
 const managerGroups = groupBy(people, "manager");
-
 const root = document.createElement("div");
 root.innerHTML = `<pre>${JSON.stringify(managerGroups, null, 2)}</pre>`;
 document.body.appendChild(root);
